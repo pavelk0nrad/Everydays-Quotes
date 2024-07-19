0f534531d3ee1166b16f5977370124cdac542ea0
Pavel Konrad <115944509+pavelk0nrad@users.noreply.github.com>
2024-07-19 13:53:39 +0200

    Basic UI and Content

--- src/App.jsx
+++ src/App.jsx
@@ -20,12 +20,20 @@ function App() {
   }
 
   return (
-    <>
-    <div className="quotes-grid">
-      {motivationalQuotes.map(createCard)}
-    </div>
+    <div className='container'>
+      <h1 className='display'>Citáty na každý den</h1>
+        <div className='today-quotes'>
+          <p>Dnešní citát: </p>
+          <p className='display-quote'> {motivationalQuotes[0].quote}</p>
+          <p> {motivationalQuotes[0].author}</p>
+          <p>Co je smyslem citátů na každý den? Každý den ti algoritmus vybere náhodný citát z databáze. I když stránku obnovíš, tento citát ti osud vybral na dnešní den. Musíš se jím tedy řídit.</p>
+          <p>Pakliže jsi netrpělivý, prohlédni si zatím databázi citátů niže.</p>
+        </div>
+        <div className="quotes-grid">
+          {motivationalQuotes.map(createCard)}
+        </div>
 
-    </>
+    </div>
   )
 }
 
