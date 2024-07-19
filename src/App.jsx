b2c4878529d0b117e4065e8696f326ae6218d943
Pavel Konrad <115944509+pavelk0nrad@users.noreply.github.com>
2024-07-19 14:18:08 +0200

    Algorithm for day normalization

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,52 @@ import './App.css'
 import Card from './components/Card'
 import motivationalQuotes from './data.js'
 
+function createCard(content){
+  return (
+    <Card 
+    id={content.id}
+    key={content.id}
+    quote={content.quote}
+    author={content.author}
+    emoji={content.emoji}
+    />
+  ) 
+}
+
 function App() {
 
-  function createCard(content){
-    return (
-      <Card 
-      id={content.id}
-      key={content.id}
-      quote={content.quote}
-      author={content.author}
-      emoji={content.emoji}
-      />
-    )
-
+  function generateScoreFromDayAndMonth() {
+    const now = new Date();
+    const dayOfWeek = now.getDay(); // Den v týdnu (0 - 6)
+    const month = now.getMonth(); // Měsíc (0 - 11)
+  
+    // Normalizace
+    const normalizedDayOfWeek = dayOfWeek / 6; // Den v týdnu (0 - 6) na (0 - 1)
+    const normalizedMonth = month / 11; // Měsíc (0 - 11) na (0 - 1)
+  
+    // Výpočet skóre jako vážený průměr
+    const score = (normalizedDayOfWeek + normalizedMonth) / 2 * 100;
+  
+    return Math.round(score); 
   }
+  
+ 
+  const result = generateScoreFromDayAndMonth();
+ 
+
+
 
   return (
     <div className='container'>
       <h1 className='display'>Citáty na každý den</h1>
         <div className='today-quotes'>
           <p>Dnešní citát: </p>
-          <p className='display-quote'> {motivationalQuotes[0].quote}</p>
-          <p> {motivationalQuotes[0].author}</p>
+          <p className='display-quote'> {motivationalQuotes[result].quote}</p>
+          <p> 
+            {motivationalQuotes[result].author}
+            
+
+          </p>
           <p>Co je smyslem citátů na každý den? Každý den ti algoritmus vybere náhodný citát z databáze. I když stránku obnovíš, tento citát ti osud vybral na dnešní den. Musíš se jím tedy řídit.</p>
           <p>Pakliže jsi netrpělivý, prohlédni si zatím databázi citátů niže.</p>
         </div>
