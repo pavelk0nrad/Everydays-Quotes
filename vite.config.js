6e501c66227215b40294b234e2cb95b24701b104
Pavel Konrad <115944509+pavelk0nrad@users.noreply.github.com>
2024-07-20 16:02:04 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://pavelk0nrad.github.io/Everydays-Quotes/',
   plugins: [react()],
 })
